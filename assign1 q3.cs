19e2a516cf01890f0e8a2e12cfa7c9cab9f09f78
Sravani294 <86480700+Sravani294@users.noreply.github.com>
2022-11-13 06:02:17 +0000

    assignment-1

---
+++ assign1 q3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass1
+{
+    internal class Ass3
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int[] num = new int[5];
+                for (int i = 0; i < num.Length; i++)
+                {
+                    num[i] = int.Parse(Console.ReadLine());
+                }
+                int addition = Ass3.Sumof(num);
+                Console.WriteLine("The sum of all the Integer Elements in the array={0}", addition);
+            }
+            catch (Exception Error)
+            {
+                Console.WriteLine("It is final");
+            }
+            Console.ReadLine();
+        }
+        public static int Sumof(params int[] num)
+        {
+            int add = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                add = add + num[i];
+            }
+            return add;
+        }
+    }
+}
