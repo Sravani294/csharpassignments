fc37aee8cbbb722dcc71667082d31c2446bc359a
Sravani294 <86480700+Sravani294@users.noreply.github.com>
2022-11-13 06:06:56 +0000

    Assignment-3

---
+++ EmpEntities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign3
+{
+    public class EmpEntities
+    {
+        public int EmpNo { get; set; }
+        public string EmpName { get; set; }
+        public double Salary { get; set; }
+        public double HRA { get; set; }
+        public double TA { get; set; }
+        public double DA { get; set; }
+        public double PF { get; set; }
+        public double TDS { get; set; }
+        public double NetSalary { get; set; }
+        public double GrossSalary { get; set; }
+        public double Petrol_allowance { get; set; }
+        public double managersalary { get; set; }
+
+        public int Mar_exec_Kms { get; set; }
+    }
+}
